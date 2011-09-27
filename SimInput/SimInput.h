5bb7b7b25bcee724348d3c2570335501a95057d9
Vincent Saluzzo <vincentsaluzzo@gmail.com>
2011-09-27 21:56:50 +0200

    Initial Commit

---
+++ SimInput/SimInput.h
@@ -0,0 +1,13 @@
+//
+//  SimInput.h
+//  SimInput
+//
+//  Created by Vincent Saluzzo on 27/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimInput : NSObject
+
+@end
---
+++ SimInput/SimInput.m
@@ -0,0 +1,23 @@
+//
+//  SimInput.m
+//  SimInput
+//
+//  Created by Vincent Saluzzo on 27/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SimInput.h"
+
+@implementation SimInput
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
